c766101f17935d318cbe253a3aa16ccc59d2f214
ddumii <tily0246@gmail.com>
2026-05-22 19:32:26 +0900

    day1 두 수를 활용한 사칙연산 함수 만들기

---
+++ day1.py
@@ -0,0 +1,35 @@
+def solution(num1, num2):
+    if  (-50000 <= num1 <= 50000) and (-50000 <= num1 <= 50000):
+        answer = num1 + num2
+        return answer
+    else:
+        print(" 값의 범위가 잘못되었습니다. ")
+
+print(solution(-50001, 5))
+
+def solution(num1, num2):
+    if  (-50000 <= num1 <= 50000) and (-50000 <= num1 <= 50000):
+        answer = num1 - num2
+        return answer
+    else:
+        print(" 값의 범위가 잘못되었습니다. ")
+
+print(solution(-50001, 5))
+
+def solution(num1, num2):
+    if  (-50000 <= num1 <= 50000) and (-50000 <= num1 <= 50000):
+        answer = num1 * num2
+        return answer
+    else:
+        print(" 값의 범위가 잘못되었습니다. ")
+
+print(solution(-50001, 5))
+
+def solution(num1, num2):
+    if  (-50000 <= num1 <= 50000) and (-50000 <= num1 <= 50000):
+        answer = num1 // num2
+        return answer
+    else:
+        print(" 값의 범위가 잘못되었습니다. ")
+
+print(solution(-50001, 5))
